08dc059522f24f2df646117f62ac0eb37d8c8f6a
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-09 11:59:22 +0100

    Beans

--- src/main/java/com/bae/AssessmentUserConsumerApplication.java
+++ src/main/java/com/bae/AssessmentUserConsumerApplication.java
@@ -1,13 +1,77 @@
 package com.bae;
 
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AssessmentUserConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssessmentUserConsumerApplication.class, args);
+	}
+
+	String BROKER_URL = "tcp://localhost:61616";
+	String BROKER_USERNAME = "admin";
+	String BROKER_PASSWORD = "admin";
+
+	@Bean
+	public ActiveMQConnectionFactory connectionFactory() {
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+
+		connectionFactory.setBrokerURL(BROKER_URL);
+		connectionFactory.setPassword(BROKER_USERNAME);
+		connectionFactory.setUserName(BROKER_PASSWORD);
+		return connectionFactory;
+	}
+
+	@Bean
+	public JmsTemplate jmsTemplate() {
+		JmsTemplate template = new JmsTemplate();
+		template.setConnectionFactory(connectionFactory());
+		return template;
+	}
+
+	@Bean
+	public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory());
+		factory.setConcurrency("1-1");
+		return factory;
+	}
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
