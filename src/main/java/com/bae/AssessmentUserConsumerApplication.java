c7403fe08b11c9c5f96db06cfa309ae34947e6a0
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-09 11:54:39 +0100

    Initial commit

---
+++ src/main/java/com/bae/AssessmentUserConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentUserConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentUserConsumerApplication.class, args);
+	}
+
+}
